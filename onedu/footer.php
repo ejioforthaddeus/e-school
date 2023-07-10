799e5448db7ecbbb960bd715e4742ce845322ef2
ejioforthadeus <ejioforthaddeus037@gmail.com>
2023-07-10 09:49:14 +0100

    project ready for use

---
+++ onedu/footer.php
@@ -0,0 +1,116 @@
+<footer>
+<div class="footer-area footer-padding">
+<div class="footer-wrapper ">
+<div class="container">
+<div class="row d-flex justify-content-between">
+<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
+<div class="single-footer-caption mb-50">
+<div class="single-footer-caption mb-30">
+
+<div class="footer-logo mb-25">
+<a href="index.html"><?php echo $name?></a>
+</div>
+<div class="footer-tittle">
+<div class="footer-pera">
+<p>The automated process starts as soon as you start with us. follow us on</p>
+</div>
+</div>
+
+<div class="footer-social">
+<a href="#"><i class="bi bi-instagram"></i></a>
+<a href="https://facebook"><i class="fab fa-facebook-f"></i></a>
+<a href="#"><i class="bi bi-youtube"></i></a>
+</div>
+</div>
+</div>
+</div>
+
+<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+<div class="single-footer-caption mb-50">
+<div class="footer-tittle">
+<h4>Newsletter</h4>
+<p>Subscribe our newsletter to get updates.</p>
+</div>
+
+<div class="footer-form">
+<div id="mc_embed_signup">
+<form target="_blank" action="" method="post" class="subscribe_form relative mail_part" novalidate="true">
+<div class="form-icon submiti">
+<a name='submit' id="savebtn" href='subscribe' type="button" class="email_icon newsletter-submit button-contactForm">
+Subscribe us
+</a>
+
+</div>
+<div class="mt-10 info"></div>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<style>
+    .submiti{
+        background-color:purple;
+        padding:10px;
+    }
+</style>
+<div class="footer-bottom-area">
+<div class="container">
+<div class="footer-border">
+<div class="row d-flex align-items-center">
+<div class="col-xl-12 ">
+<div class="footer-copy-right text-center">
+<p>Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart color-danger" aria-hidden="true"></i> by <a href="" target="_blank" rel="nofollow noopener">Pablo</a></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</footer>
+
+<div id="back-top">
+<a class="wrapper" title="Go to Top" href="#">
+<div class="arrows-container">
+<div class="arrow arrow-one">
+</div>
+<div class="arrow arrow-two">
+</div>
+</div>
+</a>
+</div>
+
+
+<script src="assets/js/vendor/modernizr-3.5.0.min.js"></script>
+<script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/slick.min.js"></script>
+<script src="assets/js/jquery.slicknav.min.js"></script>
+
+<script src="assets/js/wow.min.js"></script>
+<script src="assets/js/jquery.magnific-popup.js"></script>
+<script src="assets/js/jquery.nice-select.min.js"></script>
+<script src="assets/js/jquery.counterup.min.js"></script>
+<script src="assets/js/waypoints.min.js"></script>
+
+<script src="assets/js/contact.js"></script>
+<script src="assets/js/jquery.form.js"></script>
+<script src="assets/js/jquery.validate.min.js"></script>
+<script src="assets/js/mail-script.js"></script>
+<script src="assets/js/jquery.ajaxchimp.min.js"></script>
+
+<script src="assets/js/plugins.js"></script>
+<script src="assets/js/main.js"></script>
+<script src="assets/js/maini.js"></script>
+
+</body>
+<!-- Option 1: Include in HTML -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+<!-- Mirrored from preview.colorlib.com/theme/onedu/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 08 Jul 2023 02:17:42 GMT -->
+</html>
---
+++ onedu/header.php
@@ -0,0 +1,72 @@
+<?php 
+$name ="EJI-ELEARN"
+?>
+
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<!-- Mirrored from preview.colorlib.com/theme/onedu/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 08 Jul 2023 02:17:11 GMT -->
+<head>
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<title><?php echo $name?>|E-Education</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="shortcut icon" type="image/x-icon" href="">
+
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+<link rel="stylesheet" href="assets/css/slicknav.css">
+<link rel="stylesheet" href="assets/css/flaticon.css">
+<link rel="stylesheet" href="assets/css/animate.min.css">
+<link rel="stylesheet" href="assets/css/magnific-popup.css">
+<link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+<link rel="stylesheet" href="assets/css/themify-icons.css">
+<link rel="stylesheet" href="assets/css/slick.css">
+<link rel="stylesheet" href="assets/css/nice-select.css">
+<link rel="stylesheet" href="assets/css/style.css">
+<script nonce="085a238c-adaf-45d9-8afd-caee66edb0bb">(function(w,d){!function(f,g,h,i){f[h]=f[h]||{};f[h].executed=[];f.zaraz={deferred:[],listeners:[]};f.zaraz.q=[];f.zaraz._f=function(j){return function(){var k=Array.prototype.slice.call(arguments);f.zaraz.q.push({m:j,a:k})}};for(const l of["track","set","debug"])f.zaraz[l]=f.zaraz._f(l);f.zaraz.init=()=>{var m=g.getElementsByTagName(i)[0],n=g.createElement(i),o=g.getElementsByTagName("title")[0];o&&(f[h].t=g.getElementsByTagName("title")[0].text);f[h].x=Math.random();f[h].w=f.screen.width;f[h].h=f.screen.height;f[h].j=f.innerHeight;f[h].e=f.innerWidth;f[h].l=f.location.href;f[h].r=g.referrer;f[h].k=f.screen.colorDepth;f[h].n=g.characterSet;f[h].o=(new Date).getTimezoneOffset();if(f.dataLayer)for(const s of Object.entries(Object.entries(dataLayer).reduce(((t,u)=>({...t[1],...u[1]})),{})))zaraz.set(s[0],s[1],{scope:"page"});f[h].q=[];for(;f.zaraz.q.length;){const v=f.zaraz.q.shift();f[h].q.push(v)}n.defer=!0;for(const w of[localStorage,sessionStorage])Object.keys(w||{}).filter((y=>y.startsWith("_zaraz_"))).forEach((x=>{try{f[h]["z_"+x.slice(7)]=JSON.parse(w.getItem(x))}catch{f[h]["z_"+x.slice(7)]=w.getItem(x)}}));n.referrerPolicy="origin";n.src="../../cdn-cgi/zaraz/sd0d9.js?z="+btoa(encodeURIComponent(JSON.stringify(f[h])));m.parentNode.insertBefore(n,m)};["complete","interactive"].includes(g.readyState)?zaraz.init():f.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,"zarazData","script");})(window,document);</script></head>
+<body>
+<header>
+<div class="header-area">
+<div class="main-header header-sticky">
+<div class="container-fluid">
+<div class="menu-wrapper d-flex align-items-center justify-content-between">
+<div class="left-content d-flex align-items-center">
+<div class="logo">
+<a href="index" class="echo"><?php echo $name?></a>
+</div>
+
+</div>
+
+<style>
+.echo{
+    color:purple;
+    font-size:20px;
+}
+</style>
+<div class="main-menu d-none d-lg-block">
+<nav>
+<ul id="navigation">
+<li><a href="index">Home</a></li>
+<li><a href="courses">Browse Courses</a></li>
+<li><a href="about">About</a></li>
+</li>
+<li><a href="contact">Contact</a></li>
+<li><a href="subscribe">subscribe us</a></li>
+
+<li>
+<a href="login" class="btn header-btn2">Sign In</a>
+</li>
+</ul>
+</nav>
+</div>
+</div>
+
+<div class="col-12">
+<div class="mobile_menu d-block d-lg-none"></div>
+</div>
+</div>
+</div>
+</div>
+</header>
